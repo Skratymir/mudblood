6a061785289752c95cec21ef1639b82da628781c
Skratymir <97696626+Skratymir@users.noreply.github.com>
2022-09-02 19:06:06 +0200

    Squashed commit of the following:
    commit 020a8c404135572864973c6d925bc115035a29f0
    Merge: 92fa226 973e6c8
    Author: Skratymir <97696626+Skratymir@users.noreply.github.com>
    Date:   Fri Sep 2 19:03:31 2022 +0200
    
        Merge main into player_login to resolve conflicts
    
    commit 92fa22617767fba83423c637463512ce46de3f4d
    Author: Skratymir <97696626+Skratymir@users.noreply.github.com>
    Date:   Fri Sep 2 18:52:41 2022 +0200
    
        Added player login
    
    commit 54824eb6bfc097fbf34ca7310d22db05f5d3c557
    Author: Skratymir <97696626+Skratymir@users.noreply.github.com>
    Date:   Thu Sep 1 17:55:55 2022 +0200
    
        Added function to save player data
    
    commit 14ea6992615ecaf924ecea21012112d6eb71dd4a
    Author: Skratymir <97696626+Skratymir@users.noreply.github.com>
    Date:   Thu Sep 1 17:55:39 2022 +0200
    
        Fixed saving room with list position
        Before: [0, 0].json, After: 0-0.json
    
    commit cd70594cea522409eef43160d9d10b9b3f43447f
    Author: Skratymir <97696626+Skratymir@users.noreply.github.com>
    Date:   Thu Sep 1 17:40:11 2022 +0200
    
        Improved documentation
    
    commit f0fab9a044990f0931556cf63c3ef5ad5c0328ff
    Author: Skratymir <97696626+Skratymir@users.noreply.github.com>
    Date:   Thu Sep 1 17:31:51 2022 +0200
    
        Removed unused import
    
    commit 148e02e4ce3ec68789ea40146c3d6b3c4386a30f
    Author: Skratymir <97696626+Skratymir@users.noreply.github.com>
    Date:   Thu Sep 1 17:31:18 2022 +0200
    
        Added playercommand "look" ""
    
    commit ead30a02bc9259d399c0c98ab992b7398ac06f0d
    Author: Skratymir <97696626+Skratymir@users.noreply.github.com>
    Date:   Thu Sep 1 17:04:12 2022 +0200
    
        Improved documentation formatting
    
    commit 49f239d7d789932b7b61778d79c349f060565b46
    Author: Skratymir <97696626+Skratymir@users.noreply.github.com>
    Date:   Thu Sep 1 14:08:18 2022 +0200
    
        Created player_commands.py
    
    commit 426504c194cfd12094dd77b911dba7ce273678f2
    Author: Skratymir <97696626+Skratymir@users.noreply.github.com>
    Date:   Thu Sep 1 14:08:00 2022 +0200
    
        Removed unused exception

--- mudblood/player_commands.py
+++ mudblood/player_commands.py
@@ -3,10 +3,11 @@ from functools import partial
 
 def look(player_id: str, context: str, player_data_directory: str, maps: list) -> str:
     """Return the look data of the specified room/object"""
+    player_name = player_utils._get_player_name(player_id, player_data_directory)
     # If no object is specified return room look data
     if context == "":
         # Get the player data
-        player_data = player_utils._get_player_data(player_id, player_data_directory)
+        player_data = player_utils._get_player_data(player_name, player_data_directory)
         # Find the map the player is currently loaded in
         for map in maps:
             if map.name == player_data["map"]:
@@ -15,7 +16,6 @@ def look(player_id: str, context: str, player_data_directory: str, maps: list) -
                 return room_data["look"]
     # Return string if no return was executed prior
     return "This hasn't been implemented yet (sry)"
-        
 
 def do_command(command: tuple, player_data_directory: str, maps: list) -> str:
     """Execute the command the player entered
