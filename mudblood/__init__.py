323ea286e244116b11c0e4551fdcb1b9628822b8
Skratymir <97696626+Skratymir@users.noreply.github.com>
2022-09-03 13:08:15 +0200

    Added main module to __init__

--- mudblood/__init__.py
+++ mudblood/__init__.py
@@ -1,2 +1,2 @@
-from . import map_utils, server, map, exceptions, player_commands, player_utils, return_codes
-__all__ = ["server", "map", "map_utils", "exceptions", "player_commands", "player_utils", "return_codes"]
+from . import map_utils, server, map, exceptions, player_commands, player_utils, return_codes, main
+__all__ = ["server", "map", "map_utils", "exceptions", "player_commands", "player_utils", "return_codes", "main"]
